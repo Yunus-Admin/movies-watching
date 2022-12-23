31dee60fd47b2a0d818276118709fee2b2540b1e
Yunus Seytnazarov <seytnazarov.yunus@gmail.com>
2022-12-23 15:22:02 +0600

    Start project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const numberOfFilms = +prompt("How many movies have you watched?", "");
+
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+const lastMovieWatched = prompt("One of the last movies watched?", "");
+const rating = +prompt("How much would you rate it?", "");
+const lastMovieWatched2 = prompt("One of the last movies watched?", "");
+const rating2 = +prompt("How much would you rate it?", "");
+
+personalMoviesDB.movies[lastMovieWatched] = rating;
+personalMoviesDB.movies[lastMovieWatched2] = rating2;
